6f9ec8e88709e760044ddc701e38c888757aee31
Vicente Woinilowicz <139511901+woinivincent@users.noreply.github.com>
2025-12-01 18:22:29 -0300

    fix linux case sensitivity

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import Profile from '../models/profile.js';
+import Profile from '../models/Profile.js';
 import { authenticate, isAdmin } from '../middleware/auth.js';
 
 const router = express.Router();
